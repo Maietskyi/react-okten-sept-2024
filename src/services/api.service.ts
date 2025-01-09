2719299b6266d2e14edf4bc1993a4aab4a4bf3ff
Maietskyi <damaietskyi@gmail.com>
2025-01-09 14:51:08 +0200

    Add Lesson 2 / lesson 2.3 service

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -3,10 +3,10 @@ import {IUser} from "../models/IUser.ts";
 
 export const getUsers = async (): Promise<IUser[]> => {
     return await fetch('https://jsonplaceholder.typicode.com/users')
-        .then(value => value.json());;
+        .then(value => value.json());
 }
 
 export const getUser = async (id: string): Promise<IUser> => {
     return await fetch('https://jsonplaceholder.typicode.com/users' + id)
-        .then(value => value.json());;
+        .then(value => value.json());
 }
