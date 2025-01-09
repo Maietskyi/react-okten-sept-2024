fcbe75c94259d43f2f457dd9f7c469f3bc0842b0
Maietskyi <damaietskyi@gmail.com>
2025-01-09 12:19:32 +0200

    Add Lesson 2 / lesson 2.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
 import './App.css'
+import {useState} from "react";
 
-function App() {
+const App = () => {
+  // eslint-disable-next-line prefer-const
+  let [counter, setCounter]= useState<number>(0);
   return (
-    <></>
+      <div>
+        <h2>{counter}</h2>
+        <button onClick={() => {
+          setCounter(++counter);
+          console.log(counter)
+        }}>increment
+        </button>
+
+        {/*<button onClick={() => {*/}
+        {/*  setCounter(--counter);*/}
+        {/*  console.log(counter)*/}
+        {/*}}>decrement*/}
+        {/*</button>*/}
+
+        {/*Можна ще через колбек функцію*/}
+        <button onClick={() => {
+          setCounter(prevState => {
+            return --prevState;
+          });
+          console.log(counter)
+        }}>decrement
+        </button>
+      </div>
   )
 }
 
