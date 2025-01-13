5868e9bf457a4abd2ee77ded8a46c202f6e9555d
Maietskyi <damaietskyi@gmail.com>
2025-01-13 22:14:48 +0200

    Add Home Work 3 task 1

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,10 +1,11 @@
 import {Outlet} from "react-router-dom";
+import MenuComponent from "../components/menu/MenuComponent.tsx";
 
 
 export const MainLayout = () => {
     return <>
-        MainLayout
-                <Outlet/>
+        <MenuComponent/>
+        <Outlet/>
     </>
 };
 
